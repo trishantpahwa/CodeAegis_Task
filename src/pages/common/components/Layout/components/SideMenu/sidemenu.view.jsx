206d4734241bccdc25c5e23f2fb1d9482446b126
Trishant Pahwa <trishantpahwa@gmail.com>
2022-04-06 17:01:51 +0530

    Added loop for sidemenu.

--- src/pages/common/components/Layout/components/SideMenu/sidemenu.view.jsx
+++ src/pages/common/components/Layout/components/SideMenu/sidemenu.view.jsx
@@ -1,18 +1,25 @@
 import { MenuTile } from './components';
 
 export default function SideMenuView() {
+
+    const tiles = [
+        'Dashboard',
+        'Contacts',
+        'Calender',
+        'Clients',
+        'Template',
+        'Activity',
+        'Accounting',
+        'Reports',
+        'Settings',
+    ];
+    
     return (
         <div className="w-60 h-full shadow-md bg-white px-1 absolute">
             <ul className="relative">
-                <MenuTile type={'Dashboard'} />
-                <MenuTile type={'Contacts'} />
-                <MenuTile type={'Calender'} />
-                <MenuTile type={'Clients'} />
-                <MenuTile type={'Template'} />
-                <MenuTile type={'Activity'} />
-                <MenuTile type={'Accounting'} />
-                <MenuTile type={'Reports'} />
-                <MenuTile type={'Settings'} />
+                {tiles.map((tile, index) => (
+                    <MenuTile key={index} type={tile} />
+                ))}
             </ul>
         </div>
     )
